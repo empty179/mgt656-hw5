ed58bfa989eb369d7ba4cc4f92c9089fedd8b057
Evgenii K <evgenii.kozunov@yale.edu>
2016-10-06 16:47:15 +0000

    emty179 commitment

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-660!');
 });
+
+app.get('/empty179', function(request, response){
+    response.send('exuberant-rhinoceros');
+});
+
+
 
 app.listen(process.env.PORT || 4000);
